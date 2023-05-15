56e94ec496f87d64f20c7960a4b9289ae1a73823
Belal Hmeidat <belalhmeidat@icloud.com>
2023-05-16 00:09:46 +0300

    Removed finished to-do

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,7 @@ import java.util.EventListener;
 import java.util.Scanner;
 
 public class Main {
-    /*
-    TODO Add tokens, Add riddles,  Implement fill algorithm, Implement line algorithm, Add score, Add lives, *Optional*, Code cleanup, Comments
-     */
+    
     public static void main(String[] args) {
 //        Renderer renderer = new Renderer();
         Obstacle.generateObstacles();
